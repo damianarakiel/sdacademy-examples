5a7d6cbd8262c2dba12aa9e870fc7084bd85b58c
damianarakiel <damian.stefanski@hotmail.com>
2018-07-22 00:27:30 +0200

    Add CarTank - Lombok

--- 95-tdd_car/src/main/java/pl/sdacademy/tank/Car.java
+++ 95-tdd_car/src/main/java/pl/sdacademy/tank/Car.java
@@ -1,28 +1,33 @@
 package pl.sdacademy.tank;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Car {
 	private final TypeCar typeCar;
-
-	public TypeCar getTypeCar() {
-		return typeCar;
-	}
 
 	public Car(TypeCar typeCar) {
 		this.typeCar = typeCar;
 	}
 
-	public byte getMaxLevel() {
-		return maxLevel;
-	}
-
-	public byte getCurrentLevel() {
-		return currentLevel;
-	}
-
-	public void setCurrentLevel(byte currentLevel) {
-		this.currentLevel = currentLevel;
-	}
-
 	final byte maxLevel = 42;
 	byte currentLevel = 10;
+
+	//	public TypeCar getTypeCar() {
+	//		return typeCar;
+	//	}
+	//
+	//	public byte getMaxLevel() {
+	//		return maxLevel;
+	//	}
+	//
+	//	public byte getCurrentLevel() {
+	//		return currentLevel;
+	//	}
+	//
+	//	public void setCurrentLevel(byte currentLevel) {
+	//		this.currentLevel = currentLevel;
+	//	}
 }
